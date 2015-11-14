24f26b5b546fae4affe6d6a1383d313ca341ad15
Timo Uhlmann <timouhlmann@googlemail.com>
2015-11-14 11:17:01 +0100

    Working webpack config with babel

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app/scripts/entry.js',
+  output: {
+    path: 'app/scripts/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
